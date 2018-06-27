71a8b22fd5622294c2f5e52e137a5196feba50b2
Lucas Garron <code@garron.net>
2018-06-27 01:52:36 -0700

    FREEZE.indexed

---
+++ index.ts
@@ -0,0 +1,16 @@
+export {
+  KPuzzleDefinition,
+  Combine,
+  Multiply,
+  IdentityTransformation,
+  Invert,
+  EquivalentTransformations,
+  EquivalentStates,
+  Transformation,
+  default as KPuzzle,
+  SVG
+} from "./kpuzzle"
+
+export {
+  Puzzles
+} from "./puzzle_definitions"
