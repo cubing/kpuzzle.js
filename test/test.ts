bc94e8e612f4d8319e08f03145c0ced765a2c95c
Lucas Garron <code@garron.net>
2018-07-15 00:12:29 -0700

    Fix test name.

--- test/test.ts
+++ test/test.ts
@@ -4,8 +4,8 @@ import {Puzzles} from "../src/puzzle_definitions"
 
 import { expect } from "chai";
 
-describe("applyBlockMove()", () => {
-  it("should be able to apply a block move", () => {
+describe("applySiGNMove()", () => {
+  it("should be able to apply a SiGN move", () => {
     const p = new KPuzzle(Puzzles["333"]);
     p.applySiGNMove(BareSiGNMove("R", 6));
     expect(p.serialize()).to.equal("CORNERS\n7 1 2 4 3 5 6 0\n0 0 0 0 0 0 0 0\nEDGES\n0 1 2 11 7 5 6 4 8 9 10 3\n0 0 0 0 0 0 0 0 0 0 0 0");
