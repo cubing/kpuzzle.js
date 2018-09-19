9a9c81e4c7a0ee0d1192fb5fa6d10bd76cfbe194
Lucas Garron <code@garron.net>
2018-09-18 23:42:29 -0700

    Fix up test for f00ae30d and 7aa54cf2.

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,13 @@
-import {SiGNMove, BareSiGNMove} from "alg"
+import {BlockMove, BareBlockMove} from "alg"
 import {KPuzzle} from "../src/kpuzzle"
 import {Puzzles} from "../src/puzzle_definitions"
 
 import { expect } from "chai";
 
-describe("applySiGNMove()", () => {
+describe("applyBlockMove()", () => {
   it("should be able to apply a SiGN move", () => {
     const p = new KPuzzle(Puzzles["333"]);
-    p.applySiGNMove(BareSiGNMove("U", 1));
-    expect(p.state["EDGE"].permutation[0]).to.equal(3);
+    p.applyBlockMove(BareBlockMove("U", 1));
+    expect(p.state["EDGE"].permutation[0]).to.equal(1);
   });
 });
